f26f0fdd1de7bfb4af31ec76677c5f9e5be09e58
HubCreator <chocomilk8604@gmail.com>
2023-04-24 15:01:43 +0900

    refactor: @JdbcTest 구조 수정

--- src/test/java/racingcar/dao/CarDaoWebImplTest.java
+++ src/test/java/racingcar/dao/CarDaoWebImplTest.java
@@ -1,9 +1,12 @@
 package racingcar.dao;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import racingcar.dao.car.CarDaoWebImpl;
 import racingcar.dao.gameresult.GameResultDaoWebImpl;
 import racingcar.domain.Car;
@@ -13,17 +16,27 @@ import racingcar.domain.TryCount;
 import racingcar.domain.movingstrategy.DefaultMovingStrategy;
 import racingcar.utils.DtoMapper;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @JdbcTest
-@Import({CarDaoWebImpl.class, GameResultDaoWebImpl.class})
+//@Import({CarDaoWebImpl.class, GameResultDaoWebImpl.class})
 class CarDaoWebImplTest {
 
     @Autowired
-    private CarDaoWebImpl carDaoWebImpl;
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
+    private AnnotationConfigApplicationContext ac;
+
+    private CarDaoWebImpl carDaoWebImpl;
     private GameResultDaoWebImpl gameResultDaoWebImpl;
+
+
+    @BeforeEach
+    void setUp() {
+        carDaoWebImpl = new CarDaoWebImpl(jdbcTemplate);
+        gameResultDaoWebImpl = new GameResultDaoWebImpl(jdbcTemplate);
+    }
 
     @Test
     void save() {
@@ -35,5 +48,18 @@ class CarDaoWebImplTest {
         Car car = new Car("헙크", 3, true);
         Long carId = carDaoWebImpl.save(DtoMapper.mapToCarDto(gameResultId, car));
         assertThat(1L).isEqualTo(carId);
+    }
+
+
+    @Test
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " / beanDefinition = " + beanDefinition);
+            }
+        }
     }
 }
